a3182269231832a026eac7fc4d8e70043393d356
Senbonzakura1493 <fatine.benmoussa@icloud.com>
2020-10-16 10:39:58 +0200

    exo2 resolved

--- exercises/exo2/reader.go
+++ exercises/exo2/reader.go
@@ -16,6 +16,19 @@ func main() {
 	io.Copy(os.Stdout, &r)
 }
 
+func (erase spaceEraser) Read(sentence []byte) (int, error) {
+	n, err := erase.r.Read(sentence)
+	counter := 0
+	for i := 0; i < n; i++ {
+		if sentence[i] != 32 {
+			sentence[counter] = sentence[i]
+			counter++
+
+		}
+	}
+	return counter, err
+}
+
 // Implement a type that satisfies the io.Reader interface and reads from another io.Reader,
 // modifying the stream by removing the spaces.
 // Expected output: "Helloworld!"
